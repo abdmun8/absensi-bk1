9ff300e106b050907f83f0717b416d31bd852c22
Abdul Munim <abdul.munim@mst.co.id>
2023-07-02 16:00:15 +0700

    fix: favicon link

--- admin/application/views/template/index.php
+++ admin/application/views/template/index.php
@@ -3,8 +3,8 @@
 
 <head>
     <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url('asset/material-template/');?>/assets/img/apple-icon.png" />
-    <link rel="icon" type="image/png" href="<?php echo base_url('asset/material-template/');?>/assets/img/favicon.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url('asset/image/');?>favicon.png" />
+    <link rel="icon" type="image/png" href="<?php echo base_url('asset/image/');?>favicon.png" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title><?php echo (isset($_TITLE)) ? $_TITLE : '';?></title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
